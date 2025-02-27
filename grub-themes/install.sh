df369d05aa09ad6144adcad9ccdb43359eee79b5
odiak <158767461+odiak0@users.noreply.github.com>
2025-02-27 19:08:05 +0100

    fedora fix v2

--- grub-themes/install.sh
+++ grub-themes/install.sh
@@ -163,6 +163,7 @@ update_grub() {
     elif [[ -x "$(command -v grub2-mkconfig)" ]]; then
         if command -v dnf &> /dev/null; then
             sudo sed -i '/GRUB_TERMINAL_OUTPUT="console"/d' /etc/default/grub
+            sudo sed -i '/GRUB_DISABLE_RECOVERY="true"/d' /etc/default/grub
             sudo grub2-mkconfig -o /boot/grub2/grub.cfg
         else
             sudo grub2-mkconfig -o /boot/grub2/grub.cfg
